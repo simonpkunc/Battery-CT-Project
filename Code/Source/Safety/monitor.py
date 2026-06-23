ea1186eeb1c75eb283f6a909cc0d8703861c37c5
Simon Kunc <simonpkunc@gmail.com>
2026-06-23 19:13:46 +0200

    Deleted file.

--- Code/Source/Safety/monitor.py
+++
@@ -1,9 +0,0 @@
-class SafetyMonitor:
-    def __init__(self, max_temperature):
-        self.max_temperature = max_temperature
-
-    def check_temperature(self, temperature):
-        if temperature > self.max_temperature:
-            print("WARNING: Temperature too high!")
-            return False
-        return True
